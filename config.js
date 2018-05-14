063b1522ad2e0fad35409f23f293e0c7ed0f3775
Joshua Ollendick <joshua.ollendick@gmail.com>
2018-05-14 18:35:21 -0500

    add .env file to protect API key

--- config.js
+++ config.js
@@ -3,3 +3,4 @@
 exports.DATABASE_URL = process.env.DATABASE_URL || 'mongodb://localhost/instock-app';
 exports.TEST_DATABASE_URL = process.env.TEST_DATABASE_URL || 'mongodb://localhost/test-instock-app';
 exports.PORT = process.env.PORT || 8080;
+exports.SPOONACULAR_KEY = process.env.SPOONACULAR_KEY;
