63ed080e75f3c048fcf0714a95f1138c60598d4d
Joshua Ollendick <joshua.ollendick@gmail.com>
2018-05-15 17:14:04 -0500

    complete userRouter.js setup

--- users/userRouter.js
+++ users/userRouter.js
@@ -42,4 +42,65 @@ router.post('/', (req, res) => {
             location: nonTrimmedField
         });
     }
-})
+
+    const sizedFields = {
+        username: {
+            min: 1
+        },
+        password: {
+            min: 10, 
+            max: 72
+        }
+    };
+
+    const tooSmallField = Object.keys(sizedFields).find(field => 'min' in sizedFields[field] && req.body[field].trim().length < sizedFields[field].min);
+
+    const tooLargeField = Object.keys(sizedFields).find(field => 'max' in sizedFields[field] && req.body[field].trim().length > sizedFields[field].max);
+
+    if (tooSmallField || tooLargeField) {
+        return res.status(422).json({
+            code: 422,
+            reason: 'ValidationError',
+            message: tooSmallField
+                ? `Must be at least ${sizedFields[tooSmallField].min} characters long`
+                : `Must be at most ${sizedFields[tooLargeField].max} characters long`,
+            location: tooSmallField || tooLargeField
+        });
+    }
+
+    let {username, password, firstName = '', lastName = ''} = req.body;
+
+    firstName = firstName.trim();
+    lastName = lastName.trim();
+
+    return User.find({username})
+        .count()
+        .then(count => {
+            if (count > 0) {
+                return Promise.reject({
+                    code: 422,
+                    reason:'ValidationError',
+                    message: 'Username already taken',
+                    location: 'username'
+                });
+            }
+            return User.hashPassword(password);
+        })
+        .then(hash => {
+            return User.create({
+                username,
+                password: hash,
+                firstName,
+                lastName
+            });
+        })
+        .then(user => {
+            return res.status(201).json(user.serialize());
+        })
+        .cath(err => {
+            if (err.reason === 'ValidationError') {
+                return res.status(err.code).json(err);
+            }
+            res.status(500).json({code: 500, message: 'Internal server error'});
+        });
+});
