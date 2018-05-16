a99cdffe490f8fa52a7a4486202c54f3b9e04dc6
Joshua Ollendick <joshua.ollendick@gmail.com>
2018-05-15 19:44:13 -0500

    add User model

--- models.js
+++ models.js
@@ -1,5 +1,5 @@
 'use strict';
-
+const bcrypt = require('bcryptjs');
 const mongoose = require('mongoose');
 
 const pantrySchema = mongoose.Schema({
@@ -25,4 +25,37 @@ pantrySchema.methods.serialize = function() {
 
 const Pantry = mongoose.model('Pantry', pantrySchema);
 
-module.exports = {Pantry};
+const UserSchema = mongoose.Schema({
+    username: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    password: {
+        type: String, 
+        required: true
+    },
+    firstName: {type: String, default: ''},
+    lastName: {type: String, default: ''}
+});
+
+UserSchema.methods.serialize = function() {
+    return {
+        username: this.username || '',
+        firstName: this.firstName || '',
+        lastName: this.lastName || ''
+    };
+};
+
+UserSchema.methods.validatePassword = function(password) {
+    return bcrypt.compare(password, this.password);
+};
+
+UserSchema.statics.hashPassword = function(password) {
+    return bcrypt.hash(password, 10);
+};
+
+const User = mongoose.model('User', UserSchema);
+
+
+module.exports = {Pantry, User};
