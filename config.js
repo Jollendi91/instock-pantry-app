c86ca0d06451deea464de0e51b83b000bff4ac20
Joshua Ollendick <joshua.ollendick@gmail.com>
2018-05-15 20:13:38 -0500

    import JWT secret into config from .env

--- config.js
+++ config.js
@@ -4,3 +4,4 @@ exports.DATABASE_URL = process.env.DATABASE_URL || 'mongodb://localhost/instock-
 exports.TEST_DATABASE_URL = process.env.TEST_DATABASE_URL || 'mongodb://localhost/test-instock-app';
 exports.PORT = process.env.PORT || 8080;
 exports.SPOONACULAR_KEY = process.env.SPOONACULAR_KEY;
+exports.JWT_SECRET = process.env.JWT_SECRET;
