4e296d1a7c4c91f2334a04eaea8b8c83ac9dca08
Joshua Ollendick <joshua.ollendick@gmail.com>
2018-05-17 06:59:20 -0500

    create login and signup forms

---
+++ public/login.js
@@ -0,0 +1,8 @@
+'use strict';
+
+function listenforSignUpClick() {
+    $('#signup-button').click(event => {
+        event.preventDefault;
+        //Create Ajax call to endpoint that serves sign up form
+    })
+}
